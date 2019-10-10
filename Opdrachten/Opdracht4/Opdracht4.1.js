413b58969751a2155a8fcd1ad4575ffbac3efe60
MarkBout <mbout@roc-dev.com>
2019-10-10 18:55:01 +0200

    Opdracht 4.1 gemaakt 2 metodes gemaakt 1 geeft de snelheid weer en de andere toetert

---
+++ Opdrachten/Opdracht4/Opdracht4.1.js
@@ -0,0 +1,24 @@
+var auto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw",
+    snelheid: 0,
+    
+    gasgeven: function () {
+        this.snelheid += 5
+        console.log(auto.snelheid)
+    },
+    
+    toeteren: function () {
+        if (auto.snelheid == 25)
+        {
+            console.log("Toet!")
+        }
+    }
+};
+
+for (let i = 0; i < 7; i++) {
+    auto.gasgeven();
+    auto.toeteren();
+}
