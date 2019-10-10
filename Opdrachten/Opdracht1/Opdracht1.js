bb1c6830307b2f4875c2abf726a30dbc982c83dd
MarkBout <mbout.roc-dev.com>
2019-10-10 09:16:22 +0200

    Opdracht 1.1 1.2 en 1.3 gemaakt

--- Opdrachten/Opdracht1/Opdracht1.js
+++ Opdrachten/Opdracht1/Opdracht1.js
@@ -1,1 +1,15 @@
+//Opdracht 1.1
 console.log("Hello World");
+//Opdracht 1.2
+for (var i = 0; i < 10; i++) {
+    console.log("De waarde van i is " + i);
+}
+//Opdracht 1.3
+var auto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+};
+
+console.log(auto.kleur);
