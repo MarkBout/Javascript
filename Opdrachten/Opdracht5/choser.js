d4808adae0b7df8902491ef72f8ed3454f5e3a31
MarkBout <mbout@roc-dev.com>
2019-10-15 17:46:46 +0200

    Opdracht 5 gemaakt de favoriet chooser

--- Opdrachten/Opdracht5/choser.js
+++ Opdrachten/Opdracht5/choser.js
@@ -1,5 +1,6 @@
 pics = document.getElementById("holder");
 makepicholders();
+plaatjes();
 
 function makepicholders() {
     for (var i = 0; i < 9; i++) {
@@ -9,8 +10,31 @@ function makepicholders() {
         pics.appendChild(sesPicture);
     }
 }
-//picholders maken
-//pic heeft 1 plaatje en favoriet knop
 
-//fotos aam holders
-//favorietknop otevoegen
+function plaatjes() {
+    pictureholders = document.getElementsByClassName("picholder");
+    for (var i = 0; i < pictureholders.length; i++) {
+        favoriet = document.createElement("div");
+        favoriet.className = "favoriet";
+        favoriet.id = "favoriet " + (i+1);
+        obamaPrism = document.createElement("img");
+        obamaPrism.src = "img/" + (i+1) + ".png";
+        obamaPrism.id = (i + 1);
+        obamaPrism.addEventListener("click", function () {
+            hartje(this.id);
+        });
+        pictureholders[i].appendChild(favoriet);
+        pictureholders[i].appendChild(obamaPrism);
+    }
+}
+
+function hartje(id) {
+    console.log("Het id nummer is "+ id)
+    notfav = document.getElementsByClassName("favoriet");
+    for (var i = 0; i < notfav.length; i++) {
+        notfav[i].style.backgroundImage = "none";
+    }
+    favoriet = document.getElementById("favoriet " + id);
+    favoriet.style.backgroundImage = "url('img/heart.png')";
+
+}
